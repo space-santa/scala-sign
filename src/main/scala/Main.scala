9642c8d6dd63be2b1aea47a3ff920aa1ed05245d
claus <claus.z@protonmail.com>
2018-04-22 21:24:44 +1000

    Add functionality to main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,8 @@
 object Main {
   def main(args: Array[String]) = {
+    val limit = TerminalInfo.width
+    val lines = new Lines(limit, args)
+    val sign = new Sign(lines)
+    print(sign.get)
   }
 }
