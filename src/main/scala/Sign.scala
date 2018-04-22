a102b8cc80239829b497c10c76805de48580f97e
claus <claus.z@protonmail.com>
2018-04-22 21:00:32 +1000

    Add class Sign

---
+++ src/main/scala/Sign.scala
@@ -0,0 +1,20 @@
+class Sign(val lines: Lines) {
+    private def topOrBottom = "+" + "-" * (lines.maxLineLength + 2) + "+"
+    private def wrapLine(line: String): String = {
+        "| " + line + " " * (lines.maxLineLength - line.length) + " |"
+    }
+    private def emptyLine = wrapLine("")
+
+    def get: String = {
+        var result = topOrBottom + "\n"
+        result += emptyLine + "\n"
+
+        for (line <- lines.get) {
+            result += wrapLine(line) + "\n"
+        }
+
+        result += emptyLine + "\n"
+        result += topOrBottom
+        return result
+    }
+}
